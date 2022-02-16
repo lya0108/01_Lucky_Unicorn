30d572b952e5f94d895be99ef3b4feae30b58caa
lya0108 <lya0108@masseyhigh.school.nz>
2022-02-16 14:29:41 +1300

    Update 01_yes_no_V1.py

--- 01_yes_no_V1.py
+++ 01_yes_no_V1.py
@@ -1,15 +1,18 @@
 # Ask user if they have played before
 show_instructions = input("Have you played this game before? ").lower()
 
+yes = ["yes", "y", "ye"]
+no = ["no", "n"]
+
 # If they say yes, output 'program continues'
-if show_instructions == "yes":
+if show_instructions in yes:
     print("Program continues")
 
-elif show_instructions == "no":
+elif show_instructions in no:
     print("Display instructions")
 
 # If they say no, output 'program instructions'
 else:
     print("Please answer yes / no")
 
-print()
+print()     
