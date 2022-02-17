0583928a52d5617bac8edb30b205cb8d6ba5974b
lya0108 <lya0108@masseyhigh.school.nz>
2022-02-17 13:04:06 +1300

    Update 01_yes_no_function.py

--- 01_yes_no_function.py
+++ 01_yes_no_function.py
@@ -2,18 +2,22 @@ def yes_no(question):
     valid = False
     while not valid:
         
-        response = input("have u played before").lower()
+        yes = ["yes", "y", "ye"]
+        no = ["no", "n"]
+        
+        response = input(question).lower()
 
         # If they say yes, output 'program continues'
-        if response == "yes" or response == "y":
-            response = "yes"
-            return response
+        if response in yes:
+            return "yes"
 
 
-        elif response == "no" or response == "n":
-            response = "no"
-            return response
+        elif response in no:
+            return "no"
 
         # If they say no, output 'program instructions'
         else:
+            print()
             print("Please answer yes / no")
+
+show_instructions = yes_no("have you played before ")
