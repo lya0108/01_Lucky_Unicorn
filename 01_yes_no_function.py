239d1e9a1bd9461141ba7db2a1c6f9c4e9237a1a
lya0108 <lya0108@masseyhigh.school.nz>
2022-02-21 10:59:55 +1300

    Update 01_yes_no_function.py

--- 01_yes_no_function.py
+++ 01_yes_no_function.py
@@ -1,3 +1,6 @@
+from winsound import PlaySound
+
+
 def yes_no(question):
 
     valid = False
@@ -20,14 +23,17 @@ def yes_no(question):
             print("Please type yes / no ")
             print()
 
-# Main Routine 
-instructions = yes_no("Have u played this game before? ")
-print("you chose {}".format(instructions))
-print()
-fun = yes_no("Having fun? ")
-if fun == "no":
-    print("{} is incorrect".format(fun))
-elif fun == "yes":
-    print("You said {} to having fun".format(fun))
+def instructions():
+    print("=== How To Play ===")
+    print()
+    print("Rules")
+    print()
+    return ""
 
-    
+# Main Routine 
+played_before = yes_no("Have u played this game before? ")
+
+if played_before == "no":
+    instructions()
+
+print("pogram continues")
