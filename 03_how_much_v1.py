0e1141c86a11da21b696c7704fb640f81f7d501e
Andy <89816884+lya0108@users.noreply.github.com>
2022-02-24 13:20:32 +1300

    Add files via upload

--- 03_how_much_v1.py
+++ 03_how_much_v1.py
@@ -1,25 +1,26 @@
 # functions here
+def num_check(question, low, high):
+    error = "Please enter a whole number between 1 - 10\n"
 
+    valid = False
+    while not valid:
+        try:
 
+            # ask the question
+            response = int(input(question))
+
+            # if the amount is too low / high give
+            if low < response <= high:
+                return response
+
+            # output an error
+            else:
+                print(error)
+        
+        except ValueError:
+            print(error)
 
 # main routine
+how_much = num_check("How much would you like play with? ", 0, 10)
 
-error = "Please enter a whole number between 1 - 10\n"
-
-valid = False
-while not valid:
-    try:
-
-        # ask the question
-        response = int(input("How much would you like to play with? "))
-
-        # if the amount is too low / high give
-        if 0 < response <= 10:
-            print("You have asked to play with ${}\n".format(response))
-
-        # output an error
-        else:
-            print(error)
-    
-    except ValueError:
-        print(error)
+print("You are spending ${}\n".format(how_much))
