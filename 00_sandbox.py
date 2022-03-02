814b10833b49ce5d8c99ab98474d14b7b9606d37
Andy <89816884+lya0108@users.noreply.github.com>
2022-03-02 13:56:16 +1300

    Add files via upload

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,8 +1,1 @@
-name = ""
-while name.lower() != "xxx":
-    name = input("Who are you? ")
-    print(name)
-
-
-print()
-print("We are done! ")
+sngfsgbsbsugs
--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -5,25 +5,37 @@ STARTING_BALANCE = 100
 
 balance = STARTING_BALANCE
 # testing loop to generate 20 tokens
-for item in range(0,100):
-    chosen_num = random.randint(1,100)
+for item in range(0,10):
+    chosen_num = random.randint(1,10000)
     
     
     # adjusts balance
-    if 1 <= chosen_num <= 5:
+    if 1 <= chosen_num <= 500:
         chosen = "unicorn"
         balance += 4
-        print("unicorn")
-    elif 5 < chosen_num <= 36:
-        chosen_num == "donkey"
+    elif 3603 < chosen_num <= 4103:
+        chosen = "foal"
+        balance += 2
+    elif 500 < chosen_num <= 3600:
+        chosen = "donkey"
         balance -= 1
-        print("donkey")
+    elif 3600 < chosen_num <= 3602:
+        chosen = "mule"
+        balance -= 100
+    elif 3602 < chosen_num <= 3603:
+        chosen = "golden unicorn"
+        balance += 200
     else:
-        chosen = "horse / zebra"
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        else:
+            chosen = "zebra"
         balance -= 0.5
-        print(random("horse", "zebra")
+
+    print("you got a {}. your balance is ${:.2f}".format(chosen, balance))
 
 # output
 print()
 print("Starting Balance: ${:.2f}".format(STARTING_BALANCE))
 print("Final Balance: ${:.2f}".format(balance))
+print()
